a2d0db26c44e6ace3885fbd69b35b4c34a89e39d
Martin van Amersfoorth <martin.van.amersfoorth@finalist.nl>
2023-02-03 22:08:35 +0100

    Try another experiment with config

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -6,11 +6,10 @@ export default [
       contentSecurityPolicy: {
         useDefaults: true,
         directives: {
-          "connect-src": ["'self'", "https:"],
-          "img-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
-          "media-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
-          upgradeInsecureRequests: null,
-        },
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'dl.airtable.com', 'res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'dl.airtable.com', 'res.cloudinary.com'],
+          upgradeInsecureRequests: null,        },
       },
     },
   },
